4a59b631ae7006a8be442ddc2d022a9f2060f187
Gourav Chaturvedi <chaturvedigourav47@gmail.com>
2024-07-30 19:44:32 +0530

    Deploying the App

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express, { json } from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -12,6 +13,8 @@ import connectToDB from "./db/connectMongoDB.js";
 
 const PORT = process.env.PORT || 8000;
 
+const __dirname = path.resolve();
+
 dotenv.config();
 app.use(express.json());
 app.use(cookieParser());
@@ -24,6 +27,12 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname , "/frontend/dist")))
+
+app.get("*" , (req , res) => {
+  res.sendFile(path.join(__dirname,"frontend" , "dist" , "index.html"))
+})
 
 server.listen(PORT, () => {
   connectToDB();
